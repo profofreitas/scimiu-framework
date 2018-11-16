c87b3a5cabfe382fcf4257d959c1eb96d9a51c9d
Prof Fco. Freitas <prof.ofreitas@gmail.com>
2018-11-16 19:42:15 -0300

    add a new topic

--- src/main.cpp
+++ src/main.cpp
@@ -2,4 +2,12 @@
 
 int main(int argc, char *argv[]) {
 	std::cout << "Hello Easy C++ project!" << std::endl;
+
+	// @todo list
+	// #1. Add prototype pattern to Simulations
+	// #2. Test simple simulation.
+	// #3. Implement Vector.h and test
+	// #4. Implement VectorManager.h
+	// #5. Implemente Plotter for 2D vectors
+	// #6. Nothing to do here...
 }
